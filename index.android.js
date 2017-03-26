8dceddbbdebd3ccd7d06c9df1e9cec2c8a1771b6
Dachow <hellowd93@163.com>
2017-03-26 15:04:31 +0800

    refactor

--- index.android.js
+++ index.android.js
@@ -7,71 +7,14 @@
 import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
 
-
-// import Component
-// import MovieList from './src/MovieList';
-// import Navigation from './src/Navigation';
-// import Alert from './src/Alert';
-// import SimpleLayout from './src/Layout';
-// import SimpleIndex from './src/simpleIndex/App';
-// import StopWatch from './src/StopWatch/index';
-// import Navigation from './src/Navigation/demo'
-// import SimpleStack from './src/Navigation/SimpleStack'
-// import ModalStack from './src/Navigation/ModalStack'
-// import TabNavigator from './src/Navigation/TabNavigator'
-// import DrawerNavigator from './src/Navigation/DrawerNavigator'
-// import Animated from './src/Animated/copy'
-// import Weather from './src/Weather/index'
-// import Press from './src/Touch/Press'
-// import AwesomeMovieList from './src/MovieList/Index'
-// import GetLocation from './src/GetLocation/index'
-// import Camera from './src/Camera/index'
-// import Storage from './src/Storage/index'
-// import BDMap from './src/Map/BD'
-// import AMap from './src/Map/AMap'
-// import Alipay from './src/WebPay/Alipay';
-// import Login from './src/Login/App'
-// import WebViewTest from './src/Login/Test'
-// import Back from './src/Back/Index'
-// import LoginUI from './src/Login/UI'
-// import Signin from './src/Signin/navigator'
-// import LoginTest from './src/Login/Index'
-// import ImgPicker from './src/imgPicker/Index'
-import ImgPickMultiple from './src/imgPicker/Multiple'
-
+import App from './src/entry'
 
 // main
-class AwesomeProject extends Component {
+export default class AwesomeProject extends Component {
   render() {
-    // return <MovieList />
-    // return <Navigation />
-    // return <Alert />
-    // return <SimpleLayout />
-    // return <SimpleIndex />
-    // return <StopWatch />
-    // return <Navigation />
-    // return <SimpleStack />
-    // return <ModalStack />
-    // return <TabNavigator />
-    // return <DrawerNavigator />
-    // return <Animated />
-    // return <Weather />
-    // return <Press />
-    // return <AwesomeMovieList />
-    // return <GetLocation />
-    // return <Camera />
-    // return <Storage />
-    // return <BDMap />
-    // return <AMap />
-    // return <Alipay />
-    // return <Login />
-    // return <WebViewTest />
-    // return <Back />
-    // return <LoginUI />
-    // return <Signin />
-    // return <LoginTest />
-    // return <ImgPicker />
-    return <ImgPickMultiple />
+    return (
+      <App />
+    )
   }
 }
 
---
+++ src/entry.js
@@ -0,0 +1,75 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+
+
+// import Component
+// import MovieList from './old/MovieList';
+// import Navigation from './old/Navigation';
+// import Alert from './old/Alert';
+// import SimpleLayout from './old/Layout';
+// import SimpleIndex from './simpleIndex/App';
+// import StopWatch from './StopWatch/index';
+// import Navigation from './Navigation/demo'
+// import SimpleStack from './Navigation/SimpleStack'
+// import ModalStack from './Navigation/ModalStack'
+// import TabNavigator from './Navigation/TabNavigator'
+// import DrawerNavigator from './Navigation/DrawerNavigator'
+// import Animated from './Animated/copy'
+// import Weather from './Weather/index'
+// import Press from './Touch/Press'
+// import AwesomeMovieList from './MovieList/Index'
+// import GetLocation from './GetLocation/index'
+// import Camera from './Camera/index'
+// import Storage from './Storage/index'
+// import BDMap from './Map/BD'
+// import AMap from './Map/AMap'
+// import Alipay from './WebPay/Alipay';
+// import Login from './Login/App'
+// import WebViewTest from './Login/Test'
+// import Back from './Back/Index'
+// import LoginUI from './Login/UI'
+// import Signin from './Signin/navigator'
+// import LoginTest from './Login/Index'
+// import ImgPicker from './imgPicker/Index'
+import ImgPickMultiple from './imgPicker/Multiple'
+
+
+// main
+export default class App extends Component {
+  render() {
+    // return <MovieList />
+    // return <Navigation />
+    // return <Alert />
+    // return <SimpleLayout />
+    // return <SimpleIndex />
+    // return <StopWatch />
+    // return <Navigation />
+    // return <SimpleStack />
+    // return <ModalStack />
+    // return <TabNavigator />
+    // return <DrawerNavigator />
+    // return <Animated />
+    // return <Weather />
+    // return <Press />
+    // return <AwesomeMovieList />
+    // return <GetLocation />
+    // return <Camera />
+    // return <Storage />
+    // return <BDMap />
+    // return <AMap />
+    // return <Alipay />
+    // return <Login />
+    // return <WebViewTest />
+    // return <Back />
+    // return <LoginUI />
+    // return <Signin />
+    // return <LoginTest />
+    // return <ImgPicker />
+    return <ImgPickMultiple />
+  }
+}
